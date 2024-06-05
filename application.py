312dd0a216d25b550e58f2d5cf7e239497f7fd92
DavAnalyst <yeisonniev@gmail.com>
2024-06-04 23:19:10 -0500

    agregar html

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route('/')
 def hellow_world():
-    return 'hellow mundoo'
+    return render_template('index.html')
 
 if __name__  == "__main__":
     application.run(debug=True)
